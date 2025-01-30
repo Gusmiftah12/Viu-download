27f76b8d1492febab0aa7416e006b64292a43a07
Gusmiftah12 <gusmif@ilhamxander.web.id>
2025-01-31 03:52:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import subprocess
 
-def download_video_with_headers(m3u8_url, output_filename="output.mp4"):
-    # Headers yang ingin ditambahkan
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/vnd.apple.mpegurl',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept-Encoding': 'identity',
-        'Connection': 'Keep-Alive'
-    }
-    
-    # Format headers menjadi string yang dapat dipakai dalam ffmpeg
-    headers_string = ' '.join([f"-headers \"{key}: {value}\"" for key, value in headers.items()])
-    
-    # Command ffmpeg untuk mengunduh dan menyimpan video
-    command = f"ffmpeg -i \"{m3u8_url}\" {headers_string} -c copy {output_filename}"
-    
-    # Menjalankan perintah
-    subprocess.run(command, shell=True)
-    print(f"✅ Video saved as {output_filename}")
+url = "https://d1k2us671qcoau.cloudfront.net/vodapi/vuclip_bp.m3u8?vid=1166232456&layer=P2/video/60da9093-8a93-45ab-81b3-129414d84c08/vid_1080p_V20250126203408.m3u8&area_id=1000&lang_id=8&ts=202501310316&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kMWsydXM2NzFxY29hdS5jbG91ZGZyb250Lm5ldC92b2RhcGkvKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTczODI4MjU4OX19fV19&Signature=fiku1s1L6XyWqdAFOMlda4ZRRtOGK1lLHFMqv6dTb~eJUVDRshqrFU1DdcKJCroicEl7rdiZl5DBnWTJE0lLM5vt9uHC8lPqWZGFBcCrV9qf3olnab-MW0FN4WxwGME7Cty1jIATgNzcrcZD9VYWnaechJ5t5Ums3rhn7ae8Z673xxObRIqlB1mTDqXq8OKTP9InMIiyum8UCTVuohRXuIHlw4FrDVk5UlC0BiAye9nJltuWdpoWxyI6qDsS5JGtga9IE6Vqcrf2sSPlH957fDNLLASypd-f3raF35Idgw~f9YvHItVgyNEBhCOPRzFSj2oN~YE9oBCJpvtzunE5vw__&Key-Pair-Id=APKAJ6Z4RF5IYK7Y3SQQ"
+output_file = "file.mp4"
 
-# Contoh pemanggilan fungsi
-m3u8_url = "https://d1k2us671qcoau.cloudfront.net/vodapi/vuclip_bp.m3u8?vid=1166232457&layer=P2/video/6b6d01f2-072d-459f-990d-89295e3f5f16/vid_1080p_V20250126203418.m3u8&area_id=1000&lang_id=8&ts=202501310216&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kMWsydXM2NzFxY29hdS5jbG91ZGZyb250Lm5ldC92b2RhcGkvKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTczODI3ODk3Mn19fV19&Signature=ETnOGGFoP0d3AoSNYjm8RnHxccXbojBZTCruALKn32ver-EH0upAV3HkFeBq4nisTp8LxDimwJ5yV3abj4m-Yv3CHBbusbbdDyXx0RKSvWeoNMWnPksskGGa~eynvo3ao7YGRyH0wPCu~ibhwDeP0Ee6gGp0GRZ4O4N6U4o1WeExphZMpLlYRXzne0v2Teecwuj7TaDE8h5m-39G1N65V8tbRfbymfcj~tQqdVAq169l8QmmobJ2d0J6QN7KpBWVa7BzphWyuRcakQbpcWhk0xpas1VxwxDpdqToA587JN2uSk3mjidb0p5vjP5L8e4kFzOdv8EYet0WqccaJWfEvw__&Key-Pair-Id=APKAJ6Z4RF5IYK7Y3SQQ"
-download_video_with_headers(m3u8_url)
+command = [
+    "ffmpeg", 
+    "-i", url,
+    "-bsf:a", "aac_adtstoasc",
+    "-vcodec", "copy",
+    "-c", "copy",
+    "-crf", "50",
+    output_file
+]
+
+# Execute the command
+subprocess.run(command)
